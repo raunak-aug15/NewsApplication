3398e3a2b131ef84d793f21c031a2bba31d9bea3
raunak-aug15 <93459307+raunak-aug15@users.noreply.github.com>
2023-06-23 11:58:37 +0530

    Update News.js

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react'
 
 import NewsItem from './NewsItem'
-import Spinner from './Spinner';
+import Spinner from './Spinner'; 
 import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
 
